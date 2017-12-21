f1fefb6d29497ebfc0688956e3716cd13a6524ec
Bret Gourdie <bretgourdie@gmail.com>
2017-12-21 15:24:33 -0500

    Initial commit

---
+++ Pomodoro Logic/PomodoroTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Pomodoro_Logic
+{
+    public class PomodoroTimer
+    {
+        public int StartingMinutes { get; }
+
+        private Timer timer;
+
+        private const int NumberOfMillisecondsInOneSecond = 1000;
+
+        public PomodoroTimer(int minutes)
+        {
+            this.StartingMinutes = minutes;
+            timer = new Timer(NumberOfMillisecondsInOneSecond);
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
