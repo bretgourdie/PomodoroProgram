5012eef537d5dbcbcf061ec283571d76cedc9a68
Bret Gourdie <bretgourdie@gmail.com>
2017-12-27 14:32:31 -0500

    Create basic implementation without events

--- Pomodoro Logic/PomodoroTimer.cs
+++ Pomodoro Logic/PomodoroTimer.cs
@@ -9,30 +9,72 @@ namespace Pomodoro_Logic
 {
     public class PomodoroTimer
     {
-        public int StartingMinutes { get; }
+        public int StartingSeconds { get; private set; }
 
-        private Timer timer;
+        private int currentSeconds { get; set; }
+
+        private Timer timer { get; set; }
 
         private const int NumberOfMillisecondsInOneSecond = 1000;
 
-        public PomodoroTimer(int minutes)
+        public PomodoroTimer(int seconds)
         {
-            this.StartingMinutes = minutes;
-            timer = new Timer(NumberOfMillisecondsInOneSecond);
+            this.StartingSeconds = seconds;
+            this.timer = getTimer();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            decrementTimeRemaining();
+            throw new NotImplementedException("Publish Tick");
+            if (!IsRunning())
+            {
+                Stop();
+                throw new NotImplementedException("Publish End");
+            }
         }
 
         public void Start()
         {
-        }
-
-        public void Pause()
-        {
-
+            this.timer.Start();
         }
 
         public void Stop()
         {
+            this.timer.Stop();
+        }
 
+        public void Reset(bool stopTimer = true)
+        {
+            resetAndStopTimer(stopTimer);
+        }
+
+        private void resetAndStopTimer(bool stopTimer)
+        {
+            this.currentSeconds = this.StartingSeconds;
+
+            if (stopTimer)
+            {
+                this.timer = getTimer();
+            }
+        }
+
+        public bool IsRunning()
+        {
+            return this.currentSeconds != 0;
+        }
+
+        private void decrementTimeRemaining()
+        {
+            this.currentSeconds -= 1;
+        }
+
+        private Timer getTimer()
+        {
+            var timer = new Timer(NumberOfMillisecondsInOneSecond);
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            return timer;
         }
     }
 }
