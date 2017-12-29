efe00899be81c5f596ecdd2c46643f8de8c97a0a
Bret Gourdie <bretgourdie@gmail.com>
2017-12-29 14:03:01 -0500

    Initial add

---
+++ Pomodoro Logic/SessionConstants.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoro_Logic
+{
+    /// <summary>
+    /// Containter for constant numbers for the current session.
+    /// </summary>
+    internal class SessionConstants
+    {
+        /// <summary>
+        /// The number of seconds in one Pomodoro.
+        /// </summary>
+        public int PomodoroTimeInSeconds { get; private set; }
+        /// <summary>
+        /// The number of seconds in a short break.
+        /// </summary>
+        public int ShortBreakTimeInSeconds { get; private set; }
+        /// <summary>
+        /// The number of seconds in a long break.
+        /// </summary>
+        public int LongBreakTimeInSeconds { get; private set; }
+        /// <summary>
+        /// The number of Pomodoros per one long break.
+        /// </summary>
+        public int NumberOfPomodorosPerLongBreak { get; private set; }
+
+        /// <summary>
+        /// Creates a container for session constants.
+        /// </summary>
+        /// <param name="pomodoroTimeInSeconds">The number of seconds in one Pomodoro.</param>
+        /// <param name="shortBreakTimeInSeconds">The number of seconds in a short break.</param>
+        /// <param name="longBreakTimeInSeconds">The number of seconds in a long break.</param>
+        /// <param name="numberOfPomodorosPerLongBreak">The number of Pomodoros before a long break.</param>
+        public SessionConstants(
+            int pomodoroTimeInSeconds,
+            int shortBreakTimeInSeconds,
+            int longBreakTimeInSeconds,
+            int numberOfPomodorosPerLongBreak)
+        {
+            PomodoroTimeInSeconds = pomodoroTimeInSeconds;
+            ShortBreakTimeInSeconds = shortBreakTimeInSeconds;
+            LongBreakTimeInSeconds = longBreakTimeInSeconds;
+            NumberOfPomodorosPerLongBreak = numberOfPomodorosPerLongBreak;
+        }
+    }
+}
