77b8db4db2a5230938aeaa43abbdb83d32476085
Bret Gourdie <bretgourdie@gmail.com>
2017-12-29 14:32:39 -0500

    Implemented ShortBreakStart state

---
+++ Pomodoro Logic/ShortBreakStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoro_Logic
+{
+    public class ShortBreakStart : PomodoroStateTimer
+    {
+        public ShortBreakStart(PomodoroStateTimer oldState) : base(oldState)
+        {
+        }
+
+        protected override void createSessionTimer()
+        {
+            sessionTimer = createTimer(sessionConstants.ShortBreakTimeInSeconds);
+        }
+    }
+}
