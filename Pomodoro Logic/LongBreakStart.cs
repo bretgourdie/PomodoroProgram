3f87463cf3fc82206c6f329df84d4a3e6dc9baab
Bret Gourdie <bretgourdie@gmail.com>
2017-12-29 14:34:08 -0500

    Implemented LongBreakStart

---
+++ Pomodoro Logic/LongBreakStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoro_Logic
+{
+    public class LongBreakStart : PomodoroStateTimer
+    {
+        public LongBreakStart(PomodoroStateTimer oldState) : base(oldState)
+        {
+        }
+
+        protected override void createSessionTimer()
+        {
+            sessionTimer = createTimer(sessionConstants.LongBreakTimeInSeconds);
+        }
+    }
+}
